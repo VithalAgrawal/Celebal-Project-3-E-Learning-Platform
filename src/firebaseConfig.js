f09c6ba02e1d6b58303d0ec61c7965a6990c4017
VithalAgrawal <88157067+VithalAgrawal@users.noreply.github.com>
2023-07-23 00:44:11 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,12 @@ import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlcHM6w9-xsiUC41wQUUkpgX48yVr4oqM",
-    authDomain: "e-learning-platform-f60f8.firebaseapp.com",
-    projectId: "e-learning-platform-f60f8",
-    storageBucket: "e-learning-platform-f60f8.appspot.com",
-    messagingSenderId: "666757265441",
-    appId: "1:666757265441:web:70979b4f6ae8fcd1c58b60"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
